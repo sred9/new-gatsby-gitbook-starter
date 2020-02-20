5002b065e830903df8138aa2035beb9a3a12cbac
Sreehas Dommata <sreehasdommata98@gmail.com>
2020-02-20 19:59:49 +0530

    Update rightSidebar.js

--- src/components/rightSidebar.js
+++ src/components/rightSidebar.js
@@ -42,7 +42,7 @@ const ListItem = styled(({ className, active, level, ...props }) => {
     position: relative;
 
     &:hover {
-      color: rgb(116, 76, 188) !important;
+      color: #006400 !important;
     }
 
     ${props =>

87638abac7170bbe1715e2271cb780c1a44d2211
Sreehas Dommata <sreehasdommata98@gmail.com>
2020-03-09 22:14:25 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,4 @@
 export theme from "./theme";
 export mdxComponents from "./mdxComponents";
 export ThemeProvider from "./themeProvider";
-export Layout from "./testlayout";
 export Link from "./link";

6197900f4c7b8d54ccdbca5877cd5f603ddd302b
Sreehas Dommata <sreehasdommata98@gmail.com>
2020-03-09 20:52:24 +0530

    Create testlayout.js

---
+++ src/components/testlayout.js
@@ -0,0 +1,68 @@
+import React from "react";
+import styled from "@emotion/styled";
+import { MDXProvider } from "@mdx-js/react";
+import ThemeProvider from "./themeProvider";
+import mdxComponents from "./mdxComponents";
+import Sidebar from "./sidebar";
+import RightSidebar from "./rightSidebar";
+
+const Wrapper = styled('div')`
+  display: flex;
+  justify-content: space-between;
+
+  @media only screen and (max-width: 767px) {
+    display: block;
+  }
+`;
+
+const Content = styled('main')`
+  display: flex;
+  flex-grow: 1;
+  margin: 0px 0.35rem;
+  
+  width:58.333333%;
+  background-color:#fff;
+
+  border-radius: .25rem;
+  border: 1px solid rgba(0,0,0,.05);
+  margin-top:0.25rem;
+
+  @media only screen and (max-width: 1023px) {
+    padding-left: 0;
+    margin: 0 10px;
+    margin-top: 3rem;
+  }
+`;
+
+const MaxWidth = styled('div')`
+
+  @media only screen and (max-width: 50rem) {
+    width: 100%;
+    position: relative;
+  }
+`;
+const LeftSideBarWidth = styled('div')`
+  width: 16.666667%;
+`;
+const RightSideBarWidth = styled('div')`
+  width: 25%;
+`;
+const Layout = ({ children, location }) => (
+  <ThemeProvider location={location}>
+    <MDXProvider components={mdxComponents}>
+      <Wrapper>
+        <LeftSideBarWidth className={'hiddenMobile'}>
+          <Sidebar location={location} />
+        </LeftSideBarWidth>
+        <Content>
+          <MaxWidth>{children}</MaxWidth>
+        </Content>
+        <RightSideBarWidth className={'hiddenMobile'}>
+          <RightSidebar location={location} />
+        </RightSideBarWidth>
+      </Wrapper>
+    </MDXProvider>
+  </ThemeProvider>
+);
+
+export default Layout;
